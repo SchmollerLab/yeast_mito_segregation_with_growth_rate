9446250ef76040a5672d102fb5414c10901df6a3
Francesco <padovaf@tcd.ie>
2025-12-03 09:33:40 +0100

    add copies of tables

--- yeast_mito_segregation/segregation_simulator/test.py
+++ yeast_mito_segregation/segregation_simulator/test.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 cwd_path = os.path.dirname(os.path.abspath(__file__))
 
-df_rel_path = r'\tables_out\start_cell_1010---_len32_final_wrong_time.csv'
+df_rel_path = r'\tables_out\start_cell_1010---_len32_final.csv'
 
 df_path = f'{cwd_path}{os.sep}{df_rel_path}'
 
